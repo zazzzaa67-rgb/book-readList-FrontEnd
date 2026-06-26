1645b9e8b9592da9d20abb851052ee7c5dd8c3d2
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-06-26 17:52:45 +0300

    commit changes

---
+++ index.js
@@ -0,0 +1,6 @@
+const letBtn = document.getElementById("let")
+letBtn.addEventListener("click" , ()=>{
+    const favorite = document.getElementById("favorite").value.trim()
+    const mood = document.getElementById("mood").value.trim()
+    const fun = document.getElementById("funOrSerious").value.trim()
+})

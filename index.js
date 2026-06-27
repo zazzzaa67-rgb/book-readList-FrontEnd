e20cefd289ac4c9416a2ec440844ae1e60206888
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-06-27 16:03:25 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const letBtn = document.getElementById("let")
-letBtn.addEventListener("click" , ()=>{
+letBtn.addEventListener("click" , async ()=>{
     const favorite = document.getElementById("favorite").value.trim()
     const mood = document.getElementById("mood").value.trim()
-    const fun = document.getElementById("funOrSerious").value.trim()
+    const funny = document.getElementById("funOrSerious").value.trim()
+    const res =  await fetch("/api/chat" , {
+        method : "POST",
+        headers:"content_type : jsonObject",
+        body : {
+            favorite : favorite,
+            userMood : mood,
+            fun : funny 
+        }
+    })
+    const data = await res.json()
+    console.log(data)
+
 })
